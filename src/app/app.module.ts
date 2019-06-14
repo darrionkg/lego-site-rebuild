ce5f980b55d8b79cbdde78ef892be1cc0b0ca0c8
Darrion Gering <darrionkg@Darrions-MacBook-Pro.local>
2019-06-14 08:15:49 -0700

    add initial files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainShopComponent } from './main-shop/main-shop.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { MainPageContentComponent } from './main-page-content/main-page-content.component';
+import { FooterContentComponent } from './footer-content/footer-content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainShopComponent,
+    NavigationComponent,
+    MainPageContentComponent,
+    FooterContentComponent
   ],
   imports: [
     BrowserModule,
