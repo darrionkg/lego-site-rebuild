e6bf2b0381e7d0fa4f88be19d22b8e77c34bb48b
Darrion Gering <darrionkg@Darrions-MacBook-Pro.local>
2019-06-21 11:54:06 -0700

    Add content to lego class

---
+++ src/app/lego.model.ts
@@ -0,0 +1,3 @@
+export class Lego {
+  constructor (public name: string, public description: string, public price: number, public rating: number, public ages: string, public pieces: number, public vipPoints: number) { }
+}
