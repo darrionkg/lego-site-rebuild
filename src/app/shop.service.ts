ddb2109a3d213a3760d51d9909aeb9193e4e6bda
Darrion Gering <darrionkg@Darrions-MacBook-Pro.local>
2019-06-21 12:24:12 -0700

    Add service and observable. Currently having version issues

---
+++ src/app/shop.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { Lego } from './lego.model';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ShopService {
+  legos: FirebaseListObservable<any[]>;
+
+  constructor(private database: AngularFireDatabase) {
+    this.legos = database.list('legos');
+   }
+
+  getLegos() {
+    return this.legos;
+  }
+
+  getLegoById(legoId: string){
+    return this.database.object('/shop/' + legoId);
+  }
+
+  addLegos(newLego) {
+    this.legos.push(newLego);
+  }
+}
--- src/app/shop/shop.component.ts
+++ src/app/shop/shop.component.ts
@@ -2,16 +2,20 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 //import { ProductService } from '../product.service';
 //import { FirebaseListObservable } from 'angularfire2/database';
+import { ShopService } from './../shop.service';
+import { Lego } from './../lego.model';
+
 @Component({
   selector: 'app-shop',
   templateUrl: './shop.component.html',
   styleUrls: ['./shop.component.css']
 })
 export class ShopComponent implements OnInit {
-
-  constructor(private router: Router) { }
+  legos: Lego[];
+  constructor(private router: Router, private shopService: ShopService) { }
 
   ngOnInit() {
+    this.legos = this.shopService.getLegos();
   }
 
   goToDetailPage(clickedLegoSet) {
